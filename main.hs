cc6600f758ab8fc0903ebbb1fee08a3244441bca
DerrickQuinnKU <47612550+DerrickQuinnKU@users.noreply.github.com>
2020-04-13 21:41:59 -0500

    Delete main.hs

--- main.hs
+++
@@ -1,9 +0,0 @@
-sorted :: [Int] -> Bool
-sorted [] = True
-sorted [a] = True
-sorted (x:y:xs) = x <= y && sorted (y:xs)
-
-sort :: [Int] -> [Int]
-sort [] = []
-sort [a] = [a]
-sort xs = sort (filter (\x -> x < maximum xs) xs ) ++ filter (\x -> x == maximum xs) xs
